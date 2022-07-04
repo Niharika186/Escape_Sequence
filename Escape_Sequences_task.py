d1640e11bbc1af7deaec3d138875115ab4a0d385
Niharika186 <107666477+Niharika186@users.noreply.github.com>
2022-07-04 18:59:56 +0530

    Add files via upload

---
+++ Escape_Sequences_task.py
@@ -0,0 +1,72 @@
+'''Small Alphabets'''
+# print('hi\akhil')      # hikhil
+# print('hi\\akhil')     # hi\akhil
+# print('hi\bablu')      # hablu
+# print('hi\\bablu')     # hi\bablu
+# print('hi\chaithanya') # hi\chaithanya
+# print('hi\dhruv')      # hi\dhruv
+# print('hi\eshwar')     # hi\eshwar      
+# print('hi\ferrari')    # hi
+#                          errari
+# print('hi\\ferrari')   # hi\ferrari
+# print('hi\ganesh')     # hi\ganesh
+# print('hi\harshith')   # hi\harshith
+# print('hi\indhu')      # hi\indhu
+# print('hi\jagadesh')   # hi\jagadesh
+# print('hi\kiran')      # hi\kiran
+# print('hi\lahari')     # hi\lahari
+# print('hi\mahesh')     # hi\mahesh
+# print('hi\niharika')   # hi
+#                          iharika
+# print('hi\\niharika')  # hi\niharika
+# print('hi\oracle')     # hi\oracle
+# print('hi\praneeth')   # hi\praneeth
+# print('hi\quarterly')  # hi\quarterly
+# print('hi\raveena')    # aveena
+# print('hi\\raveena')   # hi\raveena
+# print('hi\sahithya')   # hi\sahithya
+# print('hi\teju')       # hi      eju
+# print('hi\\teju')      # hi\teju
+# print('hi\usha')       # SyntaxError: (unicode error)
+# print('hi\\usha')      # hi\usha
+# print('hi\vagdevi')    # hi
+#                          agdevi
+# print('hi\\vagdevi')     # hi\vagdevi
+# print('hi\wesley')     # hi\wesley
+# print('hi\xmas')       # SyntaxError: (unicode error)
+# print('hi\\xmas')      # hi\xmas
+# print('hi\yahoo')      # hi\yahoo
+# print('hi\zalima')     # hi\zalima
+
+
+
+
+'''Capital Alphabets'''
+# print('hi\Akhil')      # hi\Akhil
+# print('hi\Bablu')      # hi\Bablu
+# print('hi\Chaithanya') # hi\Chaithanya
+# print('hi\Dhruv')      # hi\Dhruv
+# print('hi\Eshwar')     # hi\Eshwar     
+# print('hi\Ferrari')    # hi\Ferrari
+# print('hi\Ganesh')     # hi\Ganesh
+# print('hi\Harshith')   # hi\Harshith
+# print('hi\Indhu')      # hi\Indhu
+# print('hi\Jagadesh')   # hi\Jagadesh
+# print('hi\Kiran')      # hi\Kiran
+# print('hi\Lahari')     # hi\Lahari
+# print('hi\Mahesh')     # hi\Mahesh
+# print('hi\Niharika')   # SyntaxError: (unicode error)
+# print('hi\\Niharika')  # hi\Niharika
+# print('hi\Oracle')     # hi\Oracle
+# print('hi\Praneeth')   # hi\Praneeth
+# print('hi\Quarterly')  # hi\Quarterly
+# print('hi\Raveena')    # hi\Raveena
+# print('hi\Sahithya')   # hi\Sahithya
+# print('hi\Teju')       # hi\Teju
+# print('hi\Usha')       # SyntaxError: (unicode error)
+# print('hi\\Usha')      # hi\Usha
+# print('hi\Vagdevi')    # hi\Vagdevi
+# print('hi\Wesley')     # hi\Wesley
+# print('hi\Xmas')       # hi\Xmas
+# print('hi\Yahoo')      # hi\Yahoo
+# print('hi\Zalima')     # hi\Zalima
